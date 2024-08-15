f39887411da8079d0e9286050d621f3aeda3177c
devStr0ke <samuel.coelho@toulouse.miage.fr>
2024-08-15 04:41:25 +0200

    hero header update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { TextParallaxContentExample } from "@/components/hero/TextParallaxContent";
+import TextParallaxContentExample from "@/components/hero/TextParallaxContent";
+import dynamic from "next/dynamic";
+
+const DynamicComponent = dynamic(() => import("@/components/hero/TextParallaxContent"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main className="w-full h-[100vh]">
-      <TextParallaxContentExample />
+      <DynamicComponent />
     </main>
   );
 }
