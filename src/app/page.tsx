be62ee39b5f6a9eaa390a2fbb2d35a71a4c75023
devStr0ke <samueltomar@hotmail.com>
2024-08-15 05:47:44 +0200

    hero header update 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
   return (
     <main className="w-full h-[100vh]">
       <DynamicComponent />
+      <div className="w-full h-[100vh] bg-white">
+        <h1 className="text-white">Hello</h1>
+      </div>
     </main>
   );
 }
