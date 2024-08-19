d8f5a7ba05ce382b8e05667aef990b2bf4fddaa3
devStr0ke <samuel.coelho@toulouse.miage.fr>
2024-08-19 17:49:08 +0200

    navbar fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body className={inter.className}>
+        <Navbar />
         <section>
           {children}
         </section>
