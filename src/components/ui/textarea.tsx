8d8d1ffe79accb384d699cf0fb210fe6a8378f5b
Eva BARDEAU <evaw.bardeau@gmail.com>
2024-08-20 20:40:53 +0200

    Added Contact Page + shadcn components

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "textarea textarea-bordered textarea-lg max-w-[624px] border-b border-b-gray-300 flex min-h-[150px] w-full rounded-none bg-background py-2 placeholder:text-muted-foreground px-4 pr-20 text-gray-900 font-light text-base placeholder:font-light placeholder:text-gray-500 focus:outline-none focus:border-gray-700 sm:leading-6 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Textarea.displayName = "Textarea"
+
+export { Textarea }
