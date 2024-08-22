d7ddad94d27aed9c14fa213bc1b1d19b135568e5
devStr0ke <samuel.coelho@toulouse.miage.fr>
2024-08-22 22:28:01 +0200

    Cards UI update

--- src/app/catalogue/components/ProductCard.tsx
+++ src/app/catalogue/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Product } from "@/utils/types/products";
+import { Button } from '@/components/ui/button';
 
 interface ProductCardProps {
   product: Product;
@@ -9,25 +10,31 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product, priority = false }) => {
   return (
-    <div className="border rounded-lg overflow-hidden shadow-lg">
+    <div className="group relative block overflow-hidden border rounded-lg shadow-sm">
       <div className="relative w-full aspect-square">
         <Image
           src={product.image_url}
           alt={product.name}
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          style={{
-            objectFit: 'cover',
-          }}
-          quality={75} // Adjust this value as needed (0-100)
+          className="object-cover transition duration-300 group-hover:scale-105"
+          quality={75}
           priority={priority}
         />
       </div>
-      <div className="p-4">
-        <h3 className="font-bold text-lg mb-2">{product.name}</h3>
-        <p className="text-gray-700 text-sm mb-2">{product.type}</p>
-        <p className="text-gray-700 text-sm mb-2">{product.color}</p>
-        <p className="text-gray-900 font-bold">{product.price}€</p>
+
+      <div className="relative border-t border-gray-100 bg-white p-3">
+        <div className="flex justify-start items-center mb-1">
+          <h3 className="text-sm font-medium text-gray-900 truncate max-w-[90%]">{product.name}</h3>
+        </div>
+        <div className="flex justify-between items-center mt-2">
+          <p className="text-sm font-bold text-gray-900">{product.price}€</p>
+          <Button
+            variant="outline"
+          >
+            Ajouter au panier
+          </Button>
+        </div>
       </div>
     </div>
   );
