338faaded15fe217610626b39d9ac88127476e52
Eva BARDEAU <evaw.bardeau@gmail.com>
2024-08-23 23:35:21 +0200

    Updated Footer logo and responsive design + Added it to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/global/Navbar";
 import { CartProvider } from "@/app/context/CartContext";
+import Footer from "@/components/global/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,9 @@ export default function RootLayout({
             {children}
           </section>
         </CartProvider>
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
