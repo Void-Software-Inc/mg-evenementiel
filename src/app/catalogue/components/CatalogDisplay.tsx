b10211dbcd513b478316bfabe3e20ab1f384060e
devStr0ke <samueltomar@hotmail.com>
2024-08-24 06:01:58 +0200

    UI change
    fix

--- src/app/catalogue/components/CatalogDisplay.tsx
+++ src/app/catalogue/components/CatalogDisplay.tsx
@@ -183,7 +183,7 @@ const CatalogDisplay: React.FC = () => {
             </div>
           </div>
 
-          <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {isLoading
               ? Array.from({ length: 8 }).map((_, index) => (
                   <SkeletonProductCard key={index} />
