0ecc15c7b66f06f9a8dd4ee9dacb9f7e683a9c3d
devStr0ke <samueltomar@hotmail.com>
2024-08-27 02:29:21 +0200

    Devis display products

---
+++ src/app/devis/components/DevisDisplay.tsx
@@ -0,0 +1,11 @@
+import Stepper from './Stepper';
+
+const DevisDisplay = () => {
+  return (
+    <div className="flex flex-col items-center min-h-screen w-[90%] mx-auto mt-28 pb-20">
+      <Stepper />
+    </div>
+  );
+};
+
+export default DevisDisplay;
