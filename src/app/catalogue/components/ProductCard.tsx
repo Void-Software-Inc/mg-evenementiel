17f83484cc236f9a2e1ce83d74398a88b31a42a2
Eva BARDEAU <evaw.bardeau@gmail.com>
2024-09-06 12:29:53 +0200

    Improved catalog design + Prevented propagation on AnimatedAddButton click

--- src/app/catalogue/components/ProductCard.tsx
+++ src/app/catalogue/components/ProductCard.tsx
@@ -24,7 +24,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, priority = false }) 
     setRemainingStock(product.stock - quantity);
   }, [cart, product.id, product.stock]);
 
-  const handleQuickAdd = (removeAll: boolean) => {
+  const handleQuickAdd = (e: React.MouseEvent, removeAll: boolean) => {
+    e.preventDefault(); // Prevent the Link from being triggered
+    e.stopPropagation(); // Stop the event from bubbling up
     if (quantityInCart === 0) {
       addToCart(product, 1);
       toast.success(`${product.name} ajouté au devis`, {
@@ -44,42 +46,32 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, priority = false }) 
   };
 
   return (
-    <div className="flex flex-col space-y-2">
-      <div className="relative aspect-square overflow-hidden rounded-lg">
-        <Image
-          src={product.image_url}
-          alt={product.name}
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover transition duration-300 hover:scale-105"
-          quality={75}
-          priority={priority}
-        />
-        <div className="absolute top-2 right-2">
+    <Link href={`/catalogue/${product.id}`} className="block">
+      <div className="relative flex flex-col space-y-2 bg-zinc-100 p-4 cursor-pointer">
+        <div className="absolute top-2 right-2 z-10" onClick={(e) => e.stopPropagation()}>
           <AnimatedAddButton
-            onClick={handleQuickAdd}
+            onClick={(e, removeAll) => handleQuickAdd(e, removeAll)} // Pass removeAll correctly
             quantity={quantityInCart}
           />
         </div>
+        <div className="relative w-full max-w-[500px] max-h-[500px] aspect-square overflow-hidden rounded-lg mx-auto">
+          <Image
+            src={product.image_url}
+            alt={product.name}
+            fill
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+            className="object-contain"
+            quality={75}
+            priority={priority}
+          />
+        </div>
+        
+        <div className="w-full h-fit flex flex-col justify-start items-start space-y-1">
+          <h3 className="text-sm xl:text-lg font-light text-gray-900 truncate tracking-tight">{product.name.toUpperCase()}</h3>
+          <p className="text-sm xl:text-3xl font-light text-gray-900">{typeof product.price === 'number' ? `${product.price.toFixed(2)} €` : `À partir de ${product.price}`}</p>
+        </div>
       </div>
-      
-      <div className="flex flex-col justify-center items-center space-y-1">
-        <h3 className="text-sm font-medium text-gray-900 truncate">{product.name}</h3>
-        <p className="text-sm text-gray-500 font-semibold">{typeof product.price === 'number' ? `${product.price.toFixed(2)} €` : `À partir de ${product.price}`}</p>
-      </div>
-      
-      <div className="flex items-center space-x-2">
-        <Link href={`/catalogue/${product.id}`} className="w-full">
-          <Button
-            className="w-full text-xs font-medium text-center text-gray-700 rounded hover:bg-gray-50 transition duration-150"
-            variant="outline"
-            size="compact"
-          >
-            Voir le produit
-          </Button>
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 };
 
