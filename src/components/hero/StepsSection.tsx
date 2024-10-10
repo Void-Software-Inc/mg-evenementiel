623505c91ea097b3543dc6462373131877360dbc
Eva BARDEAU <evaw.bardeau@gmail.com>
2024-10-10 16:34:00 +0200

    Typo clicks -> clics steps section

--- src/components/hero/StepsSection.tsx
+++ src/components/hero/StepsSection.tsx
@@ -9,7 +9,7 @@ const StepsSection = () => {
     <div className='w-full h-fit flex flex-col justify-center items-center mb-20'>
       <div className="w-full max-w-4xl px-4 mb-12 flex flex-col items-center justify-center">
         <h2 className="text-zinc-800 text-center text-4xl sm:text-6xl xl:text-7xl font-extralight">
-          EN QUELQUES CLICKS
+          EN QUELQUES CLICS
         </h2>
         <p className="text-zinc-800 text-center text-lg sm:text-xl xl:text-2xl font-extralight">
           Simplifiez votre expérience événementielle en quatre étapes faciles
