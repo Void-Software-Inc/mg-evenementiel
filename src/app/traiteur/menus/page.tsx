43009f0ee2d01130d9ab00b65583d16c3e5acaed
Eva BARDEAU <evaw.bardeau@gmail.com>
2025-09-18 17:36:03 +0200

    Update design of menus page and links

--- src/app/traiteur/menus/page.tsx
+++ src/app/traiteur/menus/page.tsx
@@ -9,27 +9,33 @@ const menuCategories = {
     items: [
       {
         title: "Vin d'honneur",
-        type: "MARIAGES",
+        type: "COCKTAILS & MIGNARDISES",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/no-revisions-gA81ZTsql68-unsplash.webp?t=2025-08-24T22%3A35%3A27.340Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Vin_d_honneur_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A38%3A05.169Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Vin_d_honneur_MG_TRAITEUR.pdf"
       },
       {
         title: "Brunch",
-        type: "MARIAGES",
+        type: "LENDEMAIN DE MARIAGE",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/dylan-lu-SpwsT6b0W4A-unsplash.webp?t=2025-08-24T22%3A33%3A02.102Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Brunch_Mariage_MG_EVENEMENTS.pdf?t=2025-09-15T17%3A59%3A17.175Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Brunch_Mariage_MG_TRAITEUR.pdf"
       },
       {
         title: "Buffet dînatoire",
-        type: "MARIAGES",
+        type: "BUFFET À COMPOSER",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/kari-bjorn-photography-DZt8M38gJ38-unsplash(2).webp?t=2025-09-15T18%3A36%3A41.247Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Dinatoire_MARIAGES_MG_EVENEMENTS.pdf?t=2025-09-15T17%3A59%3A24.213Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Dinatoire_MARIAGES_MG_TRAITEUR.pdf"
       },
       {
         title: "Repas assis",
-        type: "MARIAGES",
+        type: "SUGGESTIONS DE MENUS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/robert-vasquez-OyINhnh9bbw-unsplash.webp",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Repas_Assis_Mariage_MG_EVENEMENTS.pdf?t=2025-09-15T17%3A59%3A33.399Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Repas_Assis_Mariage_MG_TRAITEUR.pdf"
+      },
+      {
+        title: "Petites Fringales",
+        type: "BUFFET POUR FIN DE SOIRÉE",
+        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/karl-joshua-bernal-N7wQG77kTB8-unsplash.webp",
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Petites_Fringales_MG_TRAITEUR.pdf"
       }
     ]
   },
@@ -37,28 +43,34 @@ const menuCategories = {
     title: "Professionnels & Particuliers",
     items: [
       {
-        title: "Brunch Particuliers",
-        type: "PARTICULIERS",
+        title: "Brunch",
+        type: "PROS & PARTICULIERS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/colin-michel-7N8amvEYF-0-unsplash(1)(1).webp?t=2025-09-15T18%3A14%3A06.240Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Brunch_particuliers_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A02%3A06.970Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Brunch_pros_particuliers_MG_TRAITEUR.pdf"
       },
       {
-        title: "Brunch Professionnels",
-        type: "PROFESSIONNELS",
+        title: "Petit déjeuner d'affaires",
+        type: "FORMULE POUR ENTREPRISES",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/redd-francisco-11KJfLNCPDU-unsplash.webp?t=2025-09-15T18%3A15%3A27.087Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Brunch_pro_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A02%3A13.485Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Petit_Dejeuner_pro_MG_TRAITEUR.pdf"
       },
       {
         title: "Buffet dînatoire",
         type: "PROS & PARTICULIERS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/imattsmart-HaXSx0UvHRE-unsplash.webp",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Dinatoire_pros_particuliers_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A02%3A18.734Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Dinatoire_pros_particuliers_MG_TRAITEUR.pdf"
+      },
+      {
+        title: "Buffet à composer",
+        type: "MIGNARDISES & BOUCHÉES",
+        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/chris-reyem-xHo_0cdo_Uo-unsplash.webp?t=2025-09-18T13%3A00%3A00.785Z",
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_a_composer_MG_TRAITEUR.pdf"
       },
       {
         title: "Repas assis",
-        type: "PROS & PARTICULIERS",
+        type: "SUGGESTIONS DE MENUS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/chuttersnap-aEnH4hJ_Mrs-unsplash(1).webp",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Repas_Assis_pros_particuliers_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A02%3A26.491Z"
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Repas_Assis_pros_particuliers_MG_TRAITEUR.pdf"
       }
     ]
   },
@@ -66,28 +78,28 @@ const menuCategories = {
     title: "Général",
     items: [
       {
-        title: "Ateliers culinaires",
-        type: "GÉNÉRAL",
-        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/israel-pina-UHJ3fOfJ-5c-unsplash.webp?t=2025-08-24T22%3A34%3A33.143Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Ateliers_Culinaires_MG_EVENEMENTS.pdf?t=2025-09-15T18%3A40%3A49.388Z"
+        title: "Menu Végétarien",
+        type: "SUGGESTIONS DE MENUS VÉGÉTARIENS",
+        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/zeynep-sude-emek-a0AL-d2y5ew-unsplash.webp?t=2025-09-18T13%3A03%3A39.515Z",
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Menu_Vegetarien_MG_TRAITEUR.pdf" 
       },
       {
-        title: "Bouchées & Mignardises",
-        type: "GÉNÉRAL",
-        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/karl-joshua-bernal-N7wQG77kTB8-unsplash.webp",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Plateaux_Bouchees_MG_EVENEMENTS.pdf?t=2025-08-24T22%3A19%3A13.239Z"
+        title: "Ateliers culinaires",
+        type: "ATELIERS INTERACTIFS SUCRÉS ET SALÉS",
+        imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/israel-pina-UHJ3fOfJ-5c-unsplash.webp?t=2025-08-24T22%3A34%3A33.143Z",
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Ateliers_Culinaires_MG_TRAITEUR.pdf"
       },
       {
         title: "Moment du gâteau & Buffet sucré",
-        type: "GÉNÉRAL",
+        type: "CARTE DES DESSERTS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/andy-quezada-OePfJ_lmY4M-unsplash.webp?t=2025-08-24T22%3A31%3A12.213Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Sucre_MG_EVENEMENTS.pdf" 
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Buffet_Sucre_MG_TRAITEUR.pdf" 
       },
       {
         title: "Carte des boissons",
-        type: "GÉNÉRAL",
+        type: "NOS BOISSONS",
         imageUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/menu%20cards/jiwon-kang-eCDtMhvexWc-unsplash.webp?t=2025-09-15T18%3A20%3A53.729Z",
-        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Carte_Boissons.pdf?t=2025-09-15T18%3A18%3A12.546Z" 
+        pdfUrl: "https://supabase.mge-dashboard.pro/storage/v1/object/public/mge-website-images/pdf-menus/Carte_Boissons_MG_TRAITEUR.pdf" 
       }
     ]
   }
